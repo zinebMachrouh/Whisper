5aff6cb840c30b6dc9cf996b063467b5040c1ea4
khadija <ladykh10@gmail.com>
2024-02-28 20:28:13 +0100

    add the last version of profile

--- resources/views/profile/partials/update-profile-information-form.blade.php
+++ resources/views/profile/partials/update-profile-information-form.blade.php
@@ -1,65 +1,74 @@
 <x-app-layout>
-<section class="p-relative h-screen">
-    <header>
-        <h2 class="text-lg font-medium text-gray-900 dark:text-gray-100">
-            {{ __('Profile Information') }}
-        </h2>
-
-        <p class="mt-1 text-sm text-gray-600 dark:text-gray-400">
-            {{ __("Update your account's profile information and email address.") }}
-        </p>
-    </header>
-    <form id="send-verification" method="post" action="{{ route('verification.send') }}">
-        @csrf
-    </form>
-
-    <form method="post" action="{{ route('profile.update') }}" class="mt-6 space-y-6">
-        @csrf
-        @method('patch')
-
-        <div>
-            <x-input-label for="name" :value="__('Name')" />
-            <x-text-input id="name" name="name" type="text" class="mt-1 block w-full" :value="old('name', $user->name)" required autofocus autocomplete="name" />
-            <x-input-error class="mt-2" :messages="$errors->get('name')" />
-        </div>
-
-        <div>
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" name="email" type="email" class="mt-1 block w-full" :value="old('email', $user->email)" required autocomplete="username" />
-            <x-input-error class="mt-2" :messages="$errors->get('email')" />
-
-            @if ($user instanceof \Illuminate\Contracts\Auth\MustVerifyEmail && ! $user->hasVerifiedEmail())
-                <div>
-                    <p class="text-sm mt-2 text-gray-800 dark:text-gray-200">
-                        {{ __('Your email address is unverified.') }}
-
-                        <button form="send-verification" class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800">
-                            {{ __('Click here to re-send the verification email.') }}
-                        </button>
-                    </p>
-
-                    @if (session('status') === 'verification-link-sent')
-                        <p class="mt-2 font-medium text-sm text-green-600 dark:text-green-400">
-                            {{ __('A new verification link has been sent to your email address.') }}
-                        </p>
-                    @endif
+<div>
+    <div class="md:grid grid-cols-4 grid-rows-2  bg-white gap-2 p-4 rounded-xl">
+         <div class="md:col-span-1 h-48 shadow-xl ">
+            <form action="{{ route('profile.update') }}" method="POST" enctype="multipart/form-data">
+                @csrf
+                @method('put')
+                 <div class="flex w-full h-full relative">
+                     <img src="{{asset('' . $user->image)}}" class="w-44 h-44 m-auto" alt="">
+                 </div>
+                <input type="file" value="{{$user->iamge}}" name="image" required>
+                <input type="hidden" value="{{$user->username}}" name="username" required>
+                <input type="hidden" value="{{$user->name}}" name="name" required>
+                <input type="hidden" value="{{$user->email}}" name="email" required>
+                <input type="hidden" name="identifiant" value="{{$user->identifiant}}" required>
+                <input type="hidden" value="{{$user->identifiant_unique}}" name="identifiant_unique" required>
+                <input type="hidden" name="age" value="{{$user->age}}">
+                <input type="hidden" name="aboutMe" value="{{$user->aboutMe}}">
+                <x-primary-button>{{ __('Save') }}</x-primary-button>
+            </form>
+         </div>
+         <div class="md:col-span-3 h-48 shadow-xl p-4 space-y-2 p-3">
+            <form action="{{ route('profile.update') }}" method="POST">
+                @csrf
+                @method('put')
+                 <div class="flex">
+                     <span
+                         class="text-sm border bg-blue-50 font-bold uppercase border-2 rounded-l px-4 py-2 bg-gray-50 whitespace-no-wrap w-2/6">Name:</span>
+                     <input
+                         class="px-4 border-l-0 cursor-default border-gray-300 focus:outline-none  rounded-md rounded-l-none shadow-sm -ml-1 w-4/6"
+                         type="text" name="name" value="{{$user->name}}" required/>
+                 </div>
+                 <div class="flex ">
+                     <span
+                         class="text-sm border bg-blue-50 font-bold uppercase border-2 rounded-l px-4 py-2 bg-gray-50 whitespace-no-wrap w-2/6">Email:</span>
+                     <input 
+                         class="px-4 border-l-0 cursor-default border-gray-300 focus:outline-none  rounded-md rounded-l-none shadow-sm -ml-1 w-4/6"
+                         type="email" name="email" value="{{$user->email}}" required/>
+                 </div>
+                  <div class="flex ">
+                     <span
+                         class="text-sm border bg-blue-50 font-bold uppercase border-2 rounded-l px-4 py-2 bg-gray-50 whitespace-no-wrap w-2/6">User Name:</span>
+                     <input 
+                         class="px-4 border-l-0 cursor-default border-gray-300 focus:outline-none  rounded-md rounded-l-none shadow-sm -ml-1 w-4/6"
+                         type="text" name="username" value="{{$user->username}}" required/>
+                 </div>
+                 <div class="flex">
+                    <span
+                        class="text-sm border bg-blue-50 font-bold uppercase border-2 rounded-l px-4 py-2 bg-gray-50 whitespace-no-wrap w-2/6">identifiant unique:</span>
+                    <input
+                        class="px-4 border-l-0 cursor-default border-gray-300 focus:outline-none  rounded-md rounded-l-none shadow-sm -ml-1 w-4/6"
+                        type="number" name="identifiant" value="{{$user->identifiant}}" required/>
                 </div>
-            @endif
-        </div>
-
-        <div class="flex items-center gap-4">
-            <x-primary-button>{{ __('Save') }}</x-primary-button>
-
-            @if (session('status') === 'profile-updated')
-                <p
-                    x-data="{ show: true }"
-                    x-show="show"
-                    x-transition
-                    x-init="setTimeout(() => show = false, 2000)"
-                    class="text-sm text-gray-600 dark:text-gray-400"
-                >{{ __('Saved.') }}</p>
-            @endif
-        </div>
-    </form>
-</section>
+                <div class="flex ">
+                    <span
+                        class="text-sm border bg-blue-50 font-bold uppercase border-2 rounded-l px-4 py-2 bg-gray-50 whitespace-no-wrap w-2/6">Age:</span>
+                    <input 
+                        class="px-4 border-l-0 cursor-default border-gray-300 focus:outline-none  rounded-md rounded-l-none shadow-sm -ml-1 w-4/6"
+                        type="date" name="age" value="{{$user->age}}" required/>
+                </div>
+                <div class="flex ">
+                    <span
+                        class="text-sm border bg-blue-50 font-bold uppercase border-2 rounded-l px-4 py-2 bg-gray-50 whitespace-no-wrap w-2/6">About you:</span>
+                    <input 
+                        class="px-4 border-l-0 cursor-default border-gray-300 focus:outline-none  rounded-md rounded-l-none shadow-sm -ml-1 w-4/6"
+                        type="text" name="aboutMe" value="{{$user->aboutMe}}" required/>
+                </div>
+                <input type="hidden" name="image" value="{{$user->image}}" required>
+                <x-primary-button>{{ __('Save') }}</x-primary-button>
+            </form>
+         </div>
+     </div>
+ </div>
 </x-app-layout>
