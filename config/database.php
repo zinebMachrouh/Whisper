35d0d6ee8eda5fc0d83c86fe56195408586772ea
yassir5236 <yassiraitlahmadi@gmail.com>
2024-02-29 15:13:50 +0100

    adding some functions

--- config/database.php
+++ config/database.php
@@ -66,7 +66,7 @@ return [
         'pgsql' => [
             'driver' => 'pgsql',
             'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '192.168.8.176'),
+            'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '5432'),
             'database' => env('DB_DATABASE', 'whisper2'),
             'username' => env('DB_USERNAME', 'postgres'),
